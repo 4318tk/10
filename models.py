3b936ad8759c596d908c4a2715e072d37e64de25
takeru Kudou <j4318nmo@sit.ac.jp>
2026-01-06 16:49:11 +0900

    gpu修正

--- models.py
+++ models.py
@@ -4,44 +4,50 @@ from torch import nn
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten=nn.Flatten()
-        self.network=nn.Sequential(
-            nn.Linear(28*28,512),
+        self.flatten = nn.Flatten()
+        self.network = nn.Sequential(
+            nn.Linear(28*28, 512),
             nn.ReLU(),
-            nn.Linear(512,512),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512,10)
+            nn.Linear(512, 10)
         )
-    def forward(self,x):
-        x=self.flatten(x)
-        logits=self.network(x)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.network(x)
         return logits
-    
-def test_accuracy(model,dataloader):
-    n_corrects=0
-    device=next(model.parameters()).device
+
+
+def test_accuracy(model, dataloader):
+    n_corrects = 0
+    device = next(model.parameters()).device
     model.eval()
+
     with torch.no_grad():
-        for image_batch,label_batch in dataloader:
-            image_batch=image_batch.to(device)
-            label_batch=image_batch.to(device)
-            logits_batch=model(image_batch)
+        for image_batch, label_batch in dataloader:
+            image_batch = image_batch.to(device)
+            label_batch = label_batch.to(device)
 
-            predict_batch=logits_batch.argmax(dim=1)
-            n_corrects +=(label_batch==predict_batch).sum().item()
-        accuracy=n_corrects/len(dataloader.dataset)
+            logits_batch = model(image_batch)
+            predict_batch = logits_batch.argmax(dim=1)
 
-        return accuracy
-def train (model,dataloader,loss_fn,optimizer):
+            n_corrects += (label_batch == predict_batch).sum().item()
+
+    accuracy = n_corrects / len(dataloader.dataset)
+    return accuracy
+
+
+def train(model, dataloader, loss_fn, optimizer):
     model.train()
-    device=next(model.parameters()).device
-    for image_batch,label_batch in dataloader:
-        logits_batch=model(image_batch)
+    device = next(model.parameters()).device
 
-        image_batch=image_batch.to(device)
-        label_batch=image_batch.to(device)
+    for image_batch, label_batch in dataloader:
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
 
-        loss=loss_fn(logits_batch,label_batch)
+        logits_batch = model(image_batch)
+        loss = loss_fn(logits_batch, label_batch)
 
         optimizer.zero_grad()
         loss.backward()
@@ -49,17 +55,19 @@ def train (model,dataloader,loss_fn,optimizer):
 
     return loss.item()
 
-def test(model,dataloader,loss_fn):
-    loss_total=0.0
-    device=next(model.parameters()).device
-    model.eval()
-    with torch.no_grad():
-        for imge_batch,label_batch in dataloader:
-            image_batch=image_batch.to(device)
-            label_batch=image_batch.to(device)
-            logits_batch=model(imge_batch)
 
-            loss=loss_fn(logits_batch,label_batch)
-            loss_total+=loss.item()
+def test(model, dataloader, loss_fn):
+    loss_total = 0.0
+    device = next(model.parameters()).device
+    model.eval()
+
+    with torch.no_grad():
+        for image_batch, label_batch in dataloader:
+            image_batch = image_batch.to(device)
+            label_batch = label_batch.to(device)
+
+            logits_batch = model(image_batch)
+            loss = loss_fn(logits_batch, label_batch)
+            loss_total += loss.item()
 
     return loss_total / len(dataloader)
